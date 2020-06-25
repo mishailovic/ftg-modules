9084f54ec7d34b724eef21c1292b204c91424b0c
aiexz <42418433+aiexz@users.noreply.github.com>
2020-06-25 16:01:50 +0300

    Update wttr.py

--- wttr.py
+++ wttr.py
@@ -4,27 +4,35 @@ import requests
 
 logger = logging.getLogger(__name__)
 
+
 def register(cb):
-	cb(wttr())
+    cb(WWTrMod())
 
 
 @loader.tds
-class wttr(loader.Module):
-	"""wttr.in"""
-	strings = {
-		"name": "wttr"
-	}
+class WWWTrMod(loader.Module):
+    """wttr.in"""
 
-	async def client_ready(self, client, db):
-		self.client = client
+    strings = {"name": "wttr"}
 
-	@loader.sudo
-	async def wttr(self, message):
-		""".wttr <city> for weather"""
-		message.edit("<b>Weather by wttr.in</b>")
-		city = utils.get_args(message)
-		await message.edit("Getting weather...")
-		r = requests.get("https://wttr.in/" + str(city) + "?format=%l:+%c+%t,+%w+%m")
-		await message.edit(r.text)
+    async def client_ready(self, client, db):
+        self.client = client
 
-
+    @loader.sudo
+    async def wttrcmd(self, message):
+        """.wttr <city> for weather"""
+        message.edit("<b>Weather by wttr.in</b>")
+        city = utils.get_args(message)
+        msg = []
+        if city:
+            await message.edit("Getting weather...")
+            for i in city:
+                r = requests.get(
+                    "https://wttr.in/" + i + "?format=%l:+%c+%t,+%w+%m"
+                )
+                msg.append(r.text)
+            await message.edit("".join(msg))
+        else:
+            await message.edit("Getting weather...")
+            r = requests.get("https://wttr.in/?format=%l:+%c+%t,+%w+%m")
+            await message.edit(r.text)
